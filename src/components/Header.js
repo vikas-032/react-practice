7b32a6bbd3707b1e65ff31ef84f50f29d5b8b24d
Vikas <7014.vikas@gmail.com>
2024-07-15 11:31:47 +0530

    use-State

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+
+import logo from '../assets/Logo.png'
+
+export const Header = () => {
+  return (
+    <header>
+  
+    <img src={logo} alt="Logo" />
+
+       <a href="/">Home</a>
+    </header>
+  )
+}
